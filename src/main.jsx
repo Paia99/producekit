9774d37a11592bd797d3f4ddea99a314d01565e7
Paia99 <133217085+Paia99@users.noreply.github.com>
2026-02-14 17:14:51 +0100

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import ProduceKit from "./App.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ProduceKit />
-  </React.StrictMode>
-);
